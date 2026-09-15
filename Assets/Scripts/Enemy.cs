5402869c55af10d46adf27594558a4b4b0a55a0d
Henrique Alves <henriqueadlima@hotmail.com>
2026-09-15 16:18:19 -0300

    feat: Implement knockback effect for enemies on damage taken

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private CharacterController characterController;
-    public GameObject player;
     [SerializeField] private float speed = 4f;
+    [SerializeField] private float knockBackForce = 10f;
+
+    private CharacterController characterController;
+    private Health health;
+    private bool isKnockedBack;
+    public GameObject player;
 
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        health = GetComponent<Health>();
+    }
+
+    void OnEnable()
+    {
+        health.OnDamageTaken += TakeKnockback;
+    }
+
+    void OnDisable()
+    {
+        health.OnDamageTaken -= TakeKnockback;
     }
 
     void Update()
     {
-        FollowPlayer();
+        if (!isKnockedBack) FollowPlayer();
     }
 
     private void FollowPlayer()
@@ -25,5 +41,31 @@ public class Enemy : MonoBehaviour
         Vector3 followDirection = new Vector3(targetDirection.x, 0f, targetDirection.z);
 
         characterController.Move(followDirection * Time.deltaTime * speed);
+    }
+
+    private void TakeKnockback()
+    {
+        isKnockedBack = true;
+        StartCoroutine(Knockback());
+    }
+
+    private IEnumerator Knockback()
+    {
+        Vector3 targetDirection = (transform.position - player.transform.position).normalized;
+        targetDirection.y = 0;
+
+        float duration = 0.15f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            characterController.Move(targetDirection * Time.deltaTime * knockBackForce);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        isKnockedBack = false;
     }
 }
